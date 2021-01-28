05e5dbc6171008f7dfae17919b39b3cf7039b3f0
ihupoo <840499492@qq.com>
2021-01-28 20:02:48 +0800

    init

---
+++ src/a.ts
@@ -0,0 +1,4 @@
+export default function x(){
+	console.log(2131)
+	return 123
+}
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import moduleName from './a'
+
+const b = moduleName()
+
+const x = function a(){
+	console.log('13 :>> ', 13);
+}
+
+export const a = 1
